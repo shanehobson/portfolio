21cdb32baa46d138cc25e451c67b481ced31c69f
Shane Hobson <shanehobson1@gmail.com>
2018-09-09 20:35:41 -0700

    About section first draft complete

--- src/components/About.js
+++ src/components/About.js
@@ -10,9 +10,71 @@ const About = () => {
                 <img src='/images/profile-pic2.jpg' className='about-pic' id='about-pic2'></img>
                 <img src='/images/profile-pic5.jpg' className='about-pic' id='about-pic3'></img>
             </div>
-            <p className='section-subtitle'>
-                Below are some of the projects I've completed. I've included links to the apps themselves, as well as links to the Github repository for each app. Please feel free to explore the apps as well as the source code for each app!
+            <p className='section-subtitle' id='about-subtitle'>
+                I am a front-end web developer who is passionate about designing and building user interfaces. I enjoy bringing ideas to life using web technologies.
+                I work primarily with React and Redux, although I am also proficient in vanilla Javascript. I have experience styling web pages with plain CSS, as well as with 
+                frameworks such as Bootstrap and component libraries like Material-UI. 
+                <br />
+                <br />
             </p>
+            <p className='section-subtitle' id='about-subtitle2'>
+                I am currently based in beautiful Bend, Oregon. Prior to becoming a web developer, I attended law school and worked as an attorney for three years in Indiana. 
+                I remain a licensed attorney although I no longer practice law on a regular basis. 
+                I am passionate about using my dual skillset to solve legal problems using technology.
+                For example, I have created a <a href='#portfolio' className='about-appLink'>web app</a> that allows freelance developers to generate contracts
+                to help protect them in their relationships with customers and clients.
+            </p>
+            <div className='about-listContainer'>
+                <div>
+                    <p className='about-subheading'>
+                        Languages and Frameworks:
+                    </p>
+                    <p className='about-list'>
+                        HTML<br />
+                        CSS<br />
+                        CSS Grid<br />
+                        Flexbox<br />
+                        SASS<br />
+                        Javascript<br />
+                        React<br />
+                        Redux<br />
+                        Material-UI<br />
+                        Bootstrap<br />
+                        Node.js<br />
+                        Express<br />
+                    </p>
+                </div>
+                <div>
+                    <p className='about-subheading'>
+                        Dev Tools:
+                    </p>
+                    <p className='about-list'>
+                        Visual Studio Code<br />
+                        Webpack<br />
+                        Gulp<br />
+                        Git and Github<br />
+                        Command Line/Terminal<br />
+                        Heroku<br />
+                        Google Domains<br />
+                        Amazon Web Services - Route 53<br />
+                    </p>
+                </div>
+            </div>
+            <div className='about-listContainer'>
+                <div>
+                    <p className='about-subheading'>
+                        Courses and Certificates:
+                    </p>
+                    <p className='about-list'>
+                        Team Treehouse - Front-End Web Development Certificate<br />
+                        Udemy - The Complete React Web Developer Course (With Redux)<br />
+                        Udemy - The Complete Javascript Course 2018<br />
+                        Udemy - The Complete Node.js Developer Course<br />
+                        Udemy - Bootstrap 4 From Scratch<br />
+                        Udemy - Ultimate CSS Grid Course<br />
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
   );
