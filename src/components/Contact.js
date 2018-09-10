c38b68d38585c890fd7e55345055330f38fa015b
Shane Hobson <shanehobson1@gmail.com>
2018-09-10 11:02:32 -0700

    Copy edits to content section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     <div className='contact-contentContainer'>
         <h1 className='section-title'>Get In Touch</h1>
         <p className='contact-subtitle'>
-        If you would like to create something great together, please contact me by clicking the button below!
+        Interested in working together? Please contact me by clicking the button below!
         </p>
         <div className='portfolio-portfolioButtonContainer'>
             <div className='contact-contactButton'>
